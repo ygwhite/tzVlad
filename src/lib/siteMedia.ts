9794f03265e692f85cead1b9b6559a043d7e77a2
ruslan <ru516@yandex.ru>
2026-03-28 14:08:46 +0300

    олег хуесос

---
+++ src/lib/siteMedia.ts
@@ -0,0 +1,24 @@
+import heroButcher from '../../images/fresh_red_meet_and_butchery_worker_working_in_background.jpg'
+import dairyHerd from '../../images/heads_of_black_and_white_holstein_cows_feeding_on_grass_in_stable_in_holland.jpg'
+import investorsInfrastructure from '../../images/Gemini_Generated_Image_6jywr6jywr6jywr6.png'
+import partnersLogistics from '../../images/Gemini_Generated_Image_lptbd3lptbd3lptb.png'
+import supplyNetwork from '../../images/Gemini_Generated_Image_n5p6lsn5p6lsn5p6.png'
+import agriculturalLandscape from '../../images/Gemini_Generated_Image_xngs9kxngs9kxngs.png'
+
+export const siteMedia = {
+  heroButcher,
+  dairyHerd,
+  investorsInfrastructure,
+  partnersLogistics,
+  supplyNetwork,
+  agriculturalLandscape
+}
+
+export const siteMediaAlt = {
+  heroButcher: 'Производственный цех и специалист, контролирующий качество мясного сырья',
+  dairyHerd: 'Стадо голштинов на кормах в современной ферме',
+  investorsInfrastructure: 'Вертикально интегрированная инфраструктура агропредприятия',
+  partnersLogistics: 'Логистический комплекс с современным оборудованием',
+  supplyNetwork: 'Схема поставок и инфраструктура хранения и транспортировки',
+  agriculturalLandscape: 'Поля и фермы предприятия, готового к масштабированию'
+}
